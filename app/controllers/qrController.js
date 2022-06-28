18f440f6d9a510a5da4580678d1cfa44cc9049dd
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-06-28 13:47:34 -0400

    feat: adding qr web route

--- app/controllers/qrController.js
+++ app/controllers/qrController.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
 // TODO
 const getQR = async (req, res) => {
-  res.status(200).json("hola");
+	try {
+		if (fs.existsSync('./temp/qr.png')) {
+			res.writeHead(200, { 'content-type': 'image/png' });
+			fs.createReadStream(`./temp/qr.png`).pipe(res);
+		} else {
+			res.json('sesion iniciada');
+		}
+	} catch (error) {
+		console.log(`[QR.route] ${error}`);
+	}
 };
 
 module.exports = {
-  getQR,
+	getQR,
 };
--- libs/whatsapp.js
+++ libs/whatsapp.js
@@ -1,38 +1,53 @@
-const fs = require("fs");
-const { downloadImageMessage, resize } = require("./image");
+const fs = require('fs');
+const { downloadImageMessage, resize } = require('./image');
 
 // ?Obtiene el codigo QR para iniciar la sesion
 const getQR = (base64Qrimg, asciiQR, attempts, urlCode) => {
-  console.log("Number of attempts to read the qrcode: ", attempts);
-  console.log("Terminal qrcode: ", asciiQR);
-  // console.log("base64 image string qrcode: ", base64Qrimg);
-  // console.log("urlCode (data-ref): ", urlCode);
+	console.log('Number of attempts to read the qrcode: ', attempts);
+	// console.log('Terminal qrcode: ', asciiQR);
+	// console.log('urlCode (data-ref): ', urlCode);
+	// console.log('base64 image string qrcode: ', base64);
+	const base64 = base64Qrimg.replace(/^data:image\/png;base64,/, '');
+	try {
+		fs.writeFile(`./temp/qr.png`, base64, 'base64', async err => {
+			if (err) throw err;
+		});
+	} catch (err) {
+		console.log(`[QR] ${err}`);
+	}
+};
+
+const deleteQR = () => {
+	fs.unlink(`./temp/qr.png`, err => {
+		if (err) console.error(`[QR]: ${err}`);
+	});
 };
 
 // ?Muestra el status de la sesion
-const sessionStatus = (statusSession) => console.log({ statusSession });
+const sessionStatus = statusSession => console.log({ statusSession });
 
 // ?Enviar imagen como sticker
 const generateSticker = async (client, message) => {
-  const IMAGE = await downloadImageMessage(client, message);
-  const STICKER = await resize(IMAGE);
-  console.log({ STICKER });
-  client
-    .sendImageAsSticker(message.from, `./temp/${STICKER}`)
-    .then((result) => {
-      // ?BORRA LA IMAGEN DESPUES QUE ES ENVIADA COMO STICKER
-      fs.unlink(`./temp/${STICKER}`, (err) => {
-        if (err) console.error(`[${STICKER}]: ${err}`);
-      });
-      console.log({ result });
-    })
-    .catch((erro) => {
-      console.error(`ERROR enviando el sticker[${STICKER}]: `, erro);
-    });
+	const IMAGE = await downloadImageMessage(client, message);
+	const STICKER = await resize(IMAGE);
+	console.log({ STICKER });
+	client
+		.sendImageAsSticker(message.from, `./temp/${STICKER}`)
+		.then(result => {
+			// ?BORRA LA IMAGEN DESPUES QUE ES ENVIADA COMO STICKER
+			fs.unlink(`./temp/${STICKER}`, err => {
+				if (err) console.error(`[${STICKER}]: ${err}`);
+			});
+			console.log({ result });
+		})
+		.catch(erro => {
+			console.error(`ERROR enviando el sticker[${STICKER}]: `, erro);
+		});
 };
 
 module.exports = {
-  getQR,
-  sessionStatus,
-  generateSticker,
+	getQR,
+	deleteQR,
+	sessionStatus,
+	generateSticker,
 };
